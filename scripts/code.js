7345e9e081db427dc64e74950fc98c41cac7477e
fehis <fabian.ehisehiabhi@gmail.com>
2023-06-19 20:37:11 +0200

    Styled homepage and added interactivity

--- scripts/code.js
+++ scripts/code.js
@@ -1,5 +1,17 @@
 const setup = () => {
 
+    let createButton = document.getElementById('create');
+    let loadButton = document.getElementById('load');
+
+    //redirects to page where we can create a quiz
+    createButton.addEventListener('click', () => {
+        location.href = '../createQuiz.html';
+    })
+
+    //redirects to page where we can load quizes
+    loadButton.addEventListener('click', () => {
+        location.href = '../loadQuiz.html';
+    })
 }
 
 window.addEventListener("load", setup);
